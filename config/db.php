e22aabca9f22a71369cc4823c6ac4c02edea420a
manitgithub <manit.pbac@gmail.com>
2024-03-03 11:13:31 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.42.175;dbname=test',
+    'username' => 'tran',
+    'password' => 'Aa123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
