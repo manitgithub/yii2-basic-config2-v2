7a6d39da09901003eecc30ddb067b41dae63fe14
manitgithub <manit.pbac@gmail.com>
2024-03-03 16:40:35 +0700

    Update SiteControllersTest.php

--- tests/unit/controllers/SiteControllersTest.php
+++ tests/unit/controllers/SiteControllersTest.php
@@ -19,44 +19,4 @@ class SiteControllersTest extends \Codeception\Test\Unit
         $this->assertStringContainsString('Get started with Yii', $response);
         $this->assertStringNotContainsString('Go to About', $response);
     }
-
-    public function testActionSingupUser()
-    {
-        $requert_mock = $this->createMock(Request::class);
-        $requert_mock->method('post')->willReturn([
-            'SignupForm' => [
-                'username' => 'test',
-                'email' => 'dev01@gmail',
-                'password' => '123456',
-            ]
-        ]);
-
-        Yii::$app->set('request', $requert_mock);
-
-        $controller = new SiteController('site', Yii::$app);
-        $response = $controller->actionSignup();
-
-        $this->assertArrayHasKey('response', $response);
-        $this->assertEquals('Ok', $response['response']);
-    }
-
-
-    public function testActionSingupUserFail()
-    {
-        $requert_mock = $this->createMock(Request::class);
-        $requert_mock->method('post')->willReturn([
-            'SignupForm' => [
-                'username' => 'test',
-                'email' => 'dev01@gmail',
-            ]
-        ]);
-
-        Yii::$app->set('request', $requert_mock);
-
-        $controller = new SiteController('site', Yii::$app);
-        $response = $controller->actionSignup();
-
-        $this->assertArrayHasKey('response', $response);
-        $this->assertEquals('Error', $response['response']);
-    }
 }
