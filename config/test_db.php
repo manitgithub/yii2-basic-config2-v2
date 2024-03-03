a547b34128906bb741e918670d18af52cf7ae9d4
manitgithub <manit.pbac@gmail.com>
2024-03-03 11:15:22 +0700

    Update test_db.php

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,7 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
+$db['dsn'] = 'mysql:host=192.168.42.175;dbname=test2';
+
 
 return $db;
