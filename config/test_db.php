027d4530b9fabfb9e397d2c3efa0581432b4fce2
manitgithub <manit.pbac@gmail.com>
2024-03-03 11:37:47 +0700

    update

--- config/test_db.php
+++ config/test_db.php
@@ -2,6 +2,9 @@
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=192.168.42.175;dbname=test2';
+$db['username'] = 'train';
+$db['password'] = 'Aa123456';
+
 
 
 return $db;
